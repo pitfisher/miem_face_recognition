b2766cea92966e2187ee3717f634efa94afd10d8
brewerka <113043698+brewerka@users.noreply.github.com>
2023-10-30 02:27:31 +0300

    Update frame_extractor.py

--- frame_extractor.py
+++ frame_extractor.py
@@ -1,19 +1,19 @@
-import matplotlib.pyplot as plt
 from deepface import DeepFace
 import cv2
 import os
-import numpy as np
 import argparse
 from time import perf_counter
-from datetime import datetime 
+from datetime import datetime
 
-parser = argparse.ArgumentParser(description = "frame extractor")
-parser.add_argument("-video_path", "--video_path", help = "specify path to video to extract frames from", default = "./data/test.mp4", type = str)
+parser = argparse.ArgumentParser(description="frame extractor")
+parser.add_argument("-video_path", "--video_path", help="specify path to video to extract frames from",
+                    default="C:\\Users\\milen\\test\\test2.mp4", type=str)
 args = parser.parse_args()
 
-video_path = args.video_path 
+video_path = args.video_path
 video_name = video_path.split('\\')[-1].split(".")[0]
 
+# сохраняем данные о работе программы в файл
 f = open(f"{video_name}-extraction-log.txt", "w")
 f.write(f"{video_name}.mp4 frame extraction log {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}\n")
 
@@ -21,10 +21,10 @@ isExist = os.path.exists(f"{video_name}_frames")
 if not isExist:
     os.mkdir(f"{video_name}_frames")
 
+# открываем видеофайл
 cap = cv2.VideoCapture(video_path)
-if (cap.isOpened()== False): 
-  print("Error opening video stream or file")
-
+if (cap.isOpened() == False):
+    print("Error opening video stream or file")
 
 n = 10
 n_counter = 0
@@ -32,43 +32,39 @@ saved_frame_counter = 1
 frame_counter = 0
 start_time = perf_counter()
 
-while(cap.isOpened()):
+while (cap.isOpened()):
     print(f"n_counter: {n_counter}")
     if n_counter == 0:
         frame_counter += 1
-        # Capture frame-by-frame
+        # захват кадра
         ret, frame = cap.read()
         if ret == True:
             try:
-                # Display the resulting frame
+                # обнаружение лиц на текущем кадре
                 faces = DeepFace.extract_faces(
                     img_path=frame,
                     detector_backend="retinaface",
-                    target_size= (224,224)
+                    target_size=(224, 224)
                 )
-                if(len(faces) > 1):
+                if (len(faces) > 1):
                     print("too many faces found on that frame, skipping")
                     continue
-                
-                # save the frame
+
+                # сохраненяем текущий кадр
                 cv2.imwrite(f"{video_name}_frames/{saved_frame_counter}.jpg", frame)
-                saved_frame_counter+=1
-                # Display the resulting frame
-                # cv2.imshow('Frame',frame)
-            
-                # # Press Q on keyboard to  exit
-                # if cv2.waitKey(1) & 0xFF == ord('q'):
-                #     break   
+                saved_frame_counter += 1
+
             except ValueError as e:
+                # если лицо в кадре не обнаружено
                 print(f"No face found for current frame, error message: {str(e)}")
-                n_counter = n/2
+                n_counter = n / 2
                 continue
-        # Break the loop
+        # прерываем цикл
         else:
             time_elapsed = perf_counter() - start_time
-            
-            print(f"Time elapsed: {time_elapsed}, FPS: {frame_counter/time_elapsed}\n") 
-            f.write(f"Time elapsed: {time_elapsed}, FPS: {frame_counter/time_elapsed}\n")
+
+            print(f"Time elapsed: {time_elapsed}, FPS: {frame_counter / time_elapsed}\n")
+            f.write(f"Time elapsed: {time_elapsed}, FPS: {frame_counter / time_elapsed}\n")
             print(f"frames processed: {frame_counter}")
             f.write(f"frames processed: {frame_counter}")
             print("video ended, exiting")
@@ -78,7 +74,7 @@ while(cap.isOpened()):
     else:
         n_counter -= 1
         _ = cap.grab()
- 
-# When everything done, release the video capture object
+
+# когда все сделано, освобождаем видеозахват и закрываем файл для записи данных
 cap.release()
 f.close()
